4aa9ff450b034213b2f8ee4e5c28858e996549e5
alex <alex@c02fx3fhmd6p.hitronhub.home>
2021-10-05 16:54:20 -0700

    init all submodules

--- scripts/add-submodules.sh
+++ scripts/add-submodules.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+
 git submodule init "themes/nuo"
+git submodule init "themes/even"
+git submodule init "themes/ananke"
+git submodule init "themes/bigspring-light"
 
 git submodule update
